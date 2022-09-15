359821a7a82bd581c59749ddd7f5747b918819e4
Miguel Angel Ramirez Echarry <90867689+Miguelitus87@users.noreply.github.com>
2022-09-15 00:16:23 -0300

    Add files via upload

--- src/routes/index.js
+++ src/routes/index.js
@@ -17,59 +17,43 @@ let readProductsFile = async() => {
 
 const getAll = (req,res) => {    
     res.send(products)
-
 }
 
 const findProduct = (req,res) => {
     const { id } = req.params
-    console.log(products)
-    
-    const findProduct = products.find(num => {
-        return num.id = id
-    })
+    console.log(products)   
+    const findProduct = products.find(num => {return num.id = id})
     console.log(findProduct)
     res.send(findProduct)
 }
 
 const deleteById = (req,res) => {
     const {id} = req.params;
-    
-
-    const filterArray = products.filter(num => {
-        return num.id = id
-    })
+    const filterArray = products.filter(num => {return num.id = id})
     console.log(filterArray)
     res.send(filterArray)
-
 }
 
-const addProduct = (req,res) => {  
+const addProduct = (req,res) => {
     const { body } = req
     try {
-        await readProductsFile()    
+        readProductsFile()    
         let id = 
             productsArray.length !== 0 
                 ? productsArray[productsArray.length - 1].id + 1
                 : 1
         productsArray.push({ ...body, id})
-        await fs.promises.writeFile('products.txt',JSON.stringify(productsArray))
+        fs.promises.writeFile('products.txt',JSON.stringify(productsArray))
         res.status(200).json({mensaje:'Producto agregado con exito',productos:productsArray})
     } catch (error) {
         res.status(400).json(error)
     }
 }
 
-router.get('/',(req,res)=>{
-    res.sendFile(process.cwd()+'/src/files/index.html')
-})
-
-router.get('/api/productos', getAll)
-
-router.get('/api/productos/:id', findProduct)
-
-router.post('/api/productos', dataValidation, addProduct )
-
-router.delete('/api/productos/:id', deleteById)
-
+router.get('/',(req,res)=>{res.sendFile(process.cwd()+'/files/index.html')})
+router.get('/product', getAll)
+router.get('/product/:id', findProduct)
+router.post('/product', dataValidation, addProduct )
+router.delete('/product/:id', deleteById)
 
 module.exports = router
